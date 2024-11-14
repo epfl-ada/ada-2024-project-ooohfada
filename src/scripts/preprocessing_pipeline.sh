1be460a4f5e5b906094c055a9101d137e941a1a0
Nathan Gromb <nathan.gromb@epfl.ch>
2024-11-14 20:08:12 +0100

    pipeline

--- src/scripts/preprocessing_pipeline.sh
+++ src/scripts/preprocessing_pipeline.sh
@@ -4,12 +4,19 @@ set -e # Stop the script on any error
 
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/src" # Add the src directory to the Python path
 
-source ../venv/bin/activate # Activate the virtual environment
+echo "Activating the virtual environment"
+source ../../venv/bin/activate # Activate the virtual environment
 
 echo "Running the preprocessing pipeline"
 
+echo "Perform data preprocessing"
 python data/preprocessing.py # Run the data loader
+echo "Perform BB data preprocessing"
 python data/bbdataset_preprocessing.py # Run the data loader
+echo "Perform metadata preprocessing"
 python data/reduce_metadata.py # Run the metadata reducer
 
+echo "Deactivating the virtual environment"
 deactivate # Deactivate the virtual environment
+
+echo "Preprocessing pipeline completed"
