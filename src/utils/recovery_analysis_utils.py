99033c128af07e78dc2927ff68600013ff1ee8dd
Pauline Charpentier <paulicharpe@gmail.com>
2024-11-29 17:08:47 +0100

    standardization in utils

--- src/utils/recovery_analysis_utils.py
+++ src/utils/recovery_analysis_utils.py
@@ -4,4 +4,12 @@ def match_declines(declines, timeseries, channels):
     """
     Match declines in the context of a matched observational study.
     """
+    declines = declines.copy()
     
+
+    declines['Duration'] = ( declines['Duration'] -  declines['Duration'].mean())/ declines['Duration'].std()
+    declines['Subs_start'] = ( declines['Subs_start'] -  declines['Subs_start'].mean())/ declines['Subs_start'].std()
+    declines['Views_start'] = ( declines['Views_start'] -  declines['Views_start'].mean())/ declines['Views_start'].std()
+    declines = pd.get_dummies(data = declines, columns = ['Category'], prefix = 'Cat_')
+
+    return declines
