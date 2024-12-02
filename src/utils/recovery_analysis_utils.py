69e3e266d47d7392d67d34dfef757d8d183e3c00
Nathan Gromb <nathan.gromb@epfl.ch>
2024-12-02 19:05:15 +0100

    extract tags

--- src/utils/recovery_analysis_utils.py
+++ src/utils/recovery_analysis_utils.py
@@ -3,6 +3,11 @@ import numpy as np
 import statsmodels.api as sm
 import networkx as nx
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+GREEN = '#2ca02c'
+RED = '#d62728'
 
 def match_declines(declines):
     """
@@ -57,5 +62,104 @@ def _preprocess_for_matching(declines):
     declines.rename(columns={col: col.replace(' ', '_').replace('&', '_and_') for col in declines.columns}, inplace=True)
 
     return declines
+
 def _get_similarity(propensity1, propensity2):
     return 1 - np.abs(propensity1 - propensity2)
+
+def plot_groups_by_categories(df):
+    plt.figure(figsize=(13, 4))
+    ax = plt.subplot(1, 2, 1)
+
+    # show percentage and count of recovered vs not recovered
+    counts = df['Recovered'].value_counts(normalize=False)
+
+    sns.barplot(x=counts.index, y=counts.values, hue=counts.index, palette=[RED, GREEN], legend=False)
+    plt.title('Recovery after a decline')
+    plt.xticks([0, 1], ['No', 'Yes'])
+    plt.yticks([100000, 200000, 300000], ['100k', '200k', '300k'])
+    plt.xlabel('Managed to recover from the decline')
+    plt.ylabel('Count')
+    plt.ylim(0, max(counts) * 1.1)
+
+    # add text with the percentage
+    for i, count in enumerate(counts):
+        plt.text(i, count, count, ha='center', va='bottom')
+
+    ax = plt.subplot(1, 2, 2)
+
+    # bar plot with categories
+    counts = df.groupby('Category')['Recovered'].value_counts(normalize=True).unstack().fillna(0) * 100
+    # add mean line
+    mean =  (1 - df['Recovered'].mean()) * 100
+    plt.axhline(mean, color='black', linestyle='--', linewidth=1)
+    counts.plot(kind='bar', stacked=True, color=[RED, GREEN], ax=ax, legend=False)
+    plt.title('Proportion of successful recoveries by category')
+    plt.xlabel('Category')
+    plt.ylim(0, 100)
+    plt.ylabel('Percentage')
+    plt.yticks([0, 20, 40, 60, 80, 100], ['0%', '20%', '40%', '60%', '80%', '100%'])
+
+    # put the mean on the right
+    ax_right = plt.gca().twinx()
+    ax_right.set_ylim(0, 100)
+    ax_right.set_yticks([mean])
+    ax_right.set_yticklabels([f'{mean:.2f}%'])
+    ax.legend([f'Mean over all declines', 'Not recovered', 'Recovered'], loc='lower center')
+
+    plt.show()
+
+def plot_group_distributions(df):
+    plt.figure(figsize=(14, 8))
+
+    ax = plt.subplot(2, 2, 1)
+
+    sns.histplot(data=df, x="Subs_start", hue="Recovered", log_scale=True, element="step", palette=[RED, GREEN], ax=ax)
+
+    plt.title('Distribution of channels by subscribers\nat the start of the decline')
+    plt.xlabel('Subscribers at the start of the decline')
+    plt.ylabel('Number of channels')
+
+    ax = plt.subplot(2, 2, 2)
+
+    sns.histplot(data=df, x="Views_start", hue="Recovered", log_scale=True, element="step", palette=[RED, GREEN], ax=ax)
+
+    plt.title('Distribution of channels by total number of\nviews at the start of the decline')
+    plt.xlabel('Views at the start of the decline')
+    plt.ylabel('Number of channels')
+
+    ax = plt.subplot(2, 2, 3)
+
+    sns.histplot(data=df, x="Activity_start", hue="Recovered", log_scale=True, element="step", palette=[RED, GREEN], ax=ax)
+
+    plt.title('Distribution of channels by activity\nat the start of the decline')
+    plt.xlabel('Activity at the start of the decline')
+    plt.ylabel('Number of channels')
+
+    ax = plt.subplot(2, 2, 4)
+
+    sns.histplot(data=df, x="Delta_videos", hue="Recovered", log_scale=True, element="step", palette=[RED, GREEN], ax=ax)
+
+    plt.title('Distribution of channels by delta videos\nat the start of the decline')
+    plt.xlabel('Delta videos at the start of the decline')
+    plt.ylabel('Number of channels')
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_sampling_rates(df, seed):
+    # Sample the data at different sampling rates
+    sample_proportions = np.linspace(0.01, 1, 100)
+    new_dfs = {}
+    for prop in sample_proportions:
+        new_dfs[prop] = df.sample(frac=prop, replace=False, random_state=seed)
+
+    # Plot the recovery rates
+    recovered_props = [new_dfs[prop]['Recovered'].mean() for prop in sample_proportions]
+    unrecovered_props = [1 - prop for prop in recovered_props]
+    plt.figure(figsize=(6, 2))
+    plt.plot(sample_proportions, recovered_props, label='Recovered', color=GREEN)
+    plt.plot(sample_proportions, unrecovered_props, label='Not recovered', color=RED)
+    plt.xlabel('Sample proportion')
+    plt.ylabel('Proportion of declines')
+    plt.legend()
+    plt.show()
