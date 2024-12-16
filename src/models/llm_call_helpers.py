4fc41df7795ce8d653aa898a034c0103de3f264c
nicolasteissier <nicolas.teisser@epfl.ch>
2024-12-16 14:10:06 +0100

    llm, analysis started

--- src/models/llm_call_helpers.py
+++ src/models/llm_call_helpers.py
@@ -1,7 +1,8 @@
 import ollama
 import pandas as pd
 
-GLOBAL_CONTEXT = "You are analyzing YouTube videos to determine how creators handle a decline in their channel popularity. \n The video is analyzed based on its title and description. Your task is to answer the given specific question"
+GLOBAL_CONTEXT = "You are analyzing YouTube videos to determine how creators handle a decline in their channel popularity. \n The video is analyzed based on its title. Your task is to answer the given specific question."
+TOPIC_CONTEXT = "You are analyzing YouTube channels to determine the topic based on the distribution of the 15 main tokens found with Latent Dirichlet Allocation (LDA). Your task is to identify the name of the topic based on the given distribution."
 
 def llm_pull(model):
     """
@@ -12,7 +13,7 @@ def llm_pull(model):
     """
     ollama.pull(model)
 
-def query_ollama(prompt, model="mistral"):
+def query_ollama(prompt, model="mistral:latest"):
     """
     Query the Ollama-hosted model locally
 
@@ -26,25 +27,21 @@ def query_ollama(prompt, model="mistral"):
     Raises:
     RuntimeError: If an error occurs while querying the model
     """
-
-        # Check if the model is pulled
-    if model not in ollama.list():
-        llm_pull(model)
-
     try:
         response = ollama.generate(model=model, prompt=prompt)
-        return response.get('response')
+        raw_response = response.get('response')
+        clean_response = raw_response.split()[0].replace(".", "").replace(",", "") # Keep only the first word and delete any ponctuation
+        return clean_response
     except Exception as e:
         raise RuntimeError(f"Error querying the local model: {e}")
 
-def create_prompt(context, title, description, question):
+def create_prompt(context, title, question):
     """
     Construct a prompt with context, video details, and a specific question.
 
     Parameters:
     context (str): The context of the question
     title (str): The title of the video
-    description (str): The description of the video
     question (str): The question to ask
 
     Returns:
@@ -53,100 +50,134 @@ def create_prompt(context, title, description, question):
     return (
         f"Context:\n"
         f"{context}\n\n"
-        f"Video Details:\n"
-        f"Title: {title}\n"
-        f"Description: {description}\n\n"
+        f"Video Title:\n"
+        f"{title}\n\n"
         f"Question:\n"
         f"{question}\n\n"
         f"Answer with 'True' or 'False' only."
     )
 
-def apology_video(title, description):
+def create_prompt_topic(context, main_tokens_distribution):
     """
-    Check whether the model identifies a video as an apology video from the given title and description
+    Construct a prompt with context and the distribution of the 15 main tokens found with Latent Dirichlet Allocation (LDA)
+
+    Parameters:
+    context (str): The context of the question
+    main_tokens_distribution: The distribution of the 15 main tokens found with LDA 
+
+    Returns:
+    str: The constructed prompt
+    """
+    return (
+        f"Context:\n"
+        f"{context}\n\n"
+        f"Main Tokens Distribution:\n"
+        f"{main_tokens_distribution}\n\n"
+        f"Output only the name of the topic without any sentences (1-2 words)."
+    )
+
+def apology_video(title):
+    """
+    Check whether the model identifies a video as an apology video from the given title
 
     Parameters:
     title (str): The title of the video
-    description (str): The description of the video
     """
 
-    question = "Does the title or description explicitly convey an apology or include language typically associated with taking responsibility, expressing regret, or asking for forgiveness?"
+    question = "Does the title explicitly convey an apology or include language typically associated with taking responsibility, expressing regret, or asking for forgiveness?"
 
-    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, description, question))
+    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, question))
 
-def address_decline(title, description):
+def address_decline(title):
     """
-    Check whether the model identifies a video as addressing a decline in channel popularity from the given title and description
+    Check whether the model identifies a video as addressing a decline in channel popularity from the given title
 
     Parameters:
     title (str): The title of the video
-    description (str): The description of the video
     """
 
-    question = "Does the title or description indicate that the creator is addressing a decline in their channel popularity, such as acknowledging a decrease in views, subscribers, or engagement?"
+    question = "Does the title indicate that the creator is addressing a decline in their channel popularity, such as acknowledging a decrease in views, subscribers, or engagement?"
 
-    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, description, question))
+    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, question))
 
-def announced_comeback(title, description):
+def announced_comeback(title):
     """
-    Check whether the model identifies a video as announcing a comeback from the given title and description
+    Check whether the model identifies a video as announcing a comeback from the given title
 
     Parameters:
     title (str): The title of the video
-    description (str): The description of the video
     """
 
-    question = "Does the title or description suggest that the creator is announcing a comeback, return, or new direction for their channel, such as using phrases like 'I'm back,' 'returning,' or 'new chapter'?"
+    question = "Does the title suggest that the creator is announcing a comeback, return, or new direction for their channel, such as using phrases like 'I'm back,' 'returning,' or 'new chapter'?"
 
-    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, description, question))
+    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, question))
 
-def featuring_another_creator(title, description):
+def announced_break(title):
     """
-    Check whether the model identifies a video as featuring another creator from the given title and description
+    Check whether the model identifies a video as announcing a break from the given title
 
     Parameters:
     title (str): The title of the video
-    description (str): The description of the video
     """
 
-    question = "Does the title or description mention or suggest that the video features another creator, such as a collaboration, guest appearance, or interview?"
+    question = "Does the title suggest that the creator is announcing a break, hiatus, or temporary pause in content creation, such as using phrases like 'taking a break,' 'on hiatus,' or 'need time off'?"
 
-    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, description, question))
+    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, question))
 
-def clickbait(title, description):
+def featuring_another_creator(title):
     """
-    Check whether the model identifies a video as clickbait from the given title and description
+    Check whether the model identifies a video as featuring another creator from the given title
 
     Parameters:
     title (str): The title of the video
-    description (str): The description of the video
     """
 
-    question = "Does the title or description appear to use clickbait techniques to attract viewers, such as exagerated use of caps, misleading or exaggerated claims, sensational language, or incomplete information?"
+    question = "Does the title mention or suggest that the video features another creator, such as a collaboration, guest appearance, or interview?"
 
-    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, description, question))
+    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, question))
+
+def clickbait(title):
+    """
+    Check whether the model identifies a video as clickbait from the given title
+
+    Parameters:
+    title (str): The title of the video
+    """
+
+    question = "Does the title appear to use clickbait techniques to attract viewers, such as exagerated use of caps, misleading or exaggerated claims, sensational language, or incomplete information?"
+
+    return query_ollama(create_prompt(GLOBAL_CONTEXT, title, question))
+
+def generate_topic_name(main_tokens_distribution):
+    """
+    Identify the topic name based on the distribution of the 15 main tokens found with Latent Dirichlet Allocation (LDA)
+
+    Parameters:
+    main_tokens_distribution: The distribution of the 15 main tokens found with LDA 
+
+    Returns:
+    str: The topic name generated by the LLM
+    """
+
+    return query_ollama(create_prompt_topic(TOPIC_CONTEXT, main_tokens_distribution))
 
 def __main__():
-    # Example usage
+
+    # Test the functions
     title = "I'm Sorry."
-    description = "I made a mistake and I'm sorry. I hope you can forgive me."
-    print(apology_video(title, description))
+    print(apology_video(title))
 
     title = "My Channel Update."
-    description = "I've noticed a decrease in views and subscribers lately, so I wanted to address it in this video."
-    print(address_decline(title, description))
+    print(address_decline(title))
 
     title = "I'm Back!"
-    description = "After a long break, I'm excited to announce my return to YouTube with new content."
-    print(announced_comeback(title, description))
+    print(announced_comeback(title))
 
     title = "Collab with My Friend!"
-    description = "I had a great time collaborating with my friend on this video. Check it out!"
-    print(featuring_another_creator(title, description))
+    print(featuring_another_creator(title))
 
     title = "You Won't Believe What Happened!"
-    description = "This video will shock you! Watch until the end to find out."
-    print(clickbait(title, description))
+    print(clickbait(title))
 
 if __name__ == "__main__":
     __main__()
